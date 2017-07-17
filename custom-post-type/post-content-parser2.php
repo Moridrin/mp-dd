b2549bf506b8b99f922ca30ff3f27f76844d610c
Jeroen Berkvens <jberkvens@triptic.nl>
2017-07-17 12:43:21 +0200

    Linking and pupups seem to be fully functional.

--- custom-post-type/post-content-parser2.php
+++ custom-post-type/post-content-parser2.php
@@ -17,10 +17,33 @@ function mp_dd_filter_content($content)
     global $post;
 
     if ($post->post_type == 'area') {
+        $content = mp_dd_filter_area_content($content, $post);
+    }
+    $content = mp_dd_filter_modal_tags($content);
+    return $content;
+}
+
+function mp_dd_filter_area_content($content, $post)
+{
         if (strpos($content, '[map]') === false) {
             $content = '[map]' . $content;
         }
         $content = str_replace('[map]', mp_dd_get_map($post), $content);
+    return $content;
+}
+
+function mp_dd_filter_modal_tags($content, $makeModal = true, $top = null)
+{
+    if (preg_match_all('/\[object-(\d+)-url\]/', $content, $matches)) {
+        foreach ($matches[1] as $objectID) {
+            $target = $makeModal ? "#modal_$objectID" : "#$objectID";
+            $content = str_replace("[object-$objectID-url]", $target, $content);
+            if (strpos($content, "id=\"modal_$objectID\"") === false) {
+                $objectContent = mp_dd_get_object_content($objectID, $top);
+                $objectContent = $makeModal ? mp_dd_make_modal($objectContent, $objectID) : $objectContent;
+                $content .= $objectContent;
+            }
+        }
     }
     return $content;
 }
@@ -28,9 +51,13 @@ function mp_dd_filter_content($content)
 add_filter('the_content', 'mp_dd_filter_content');
 #endregion
 
+#region Map
 function mp_dd_get_map(WP_Post $post): string
 {
     $image_id          = get_post_meta($post->ID, 'map_image_id', true);
+    if (empty($image_id)) {
+        return '';
+    }
     $image_src         = wp_get_attachment_url($image_id);
     $visibleObjects    = get_post_meta($post->ID, 'visible_objects', true);
     $visibleObjects    = is_array($visibleObjects) ? $visibleObjects : [];
@@ -62,8 +89,80 @@ function mp_dd_get_map(WP_Post $post): string
 
 function mp_dd_get_area_label_color(array $terms): string
 {
+    if (empty($terms)) {
+        return '#A0A0A0';
+    }
     /** @var WP_Term $term */
     $term = end($terms);
     $color = $term->description;
     return empty($color) ? '#A0A0A0' : $color;
 }
+
+function mp_dd_make_modal(string $content, int $objectID): string
+{
+    $modalStart = "<div id=\"modal_$objectID\" class=\"modal modal-fixed-footer\"><div id='modal_top'></div><div class=\"modal-content\">";
+    $modalEnd   = "</div></div>";
+    return $modalStart . $content . $modalEnd;
+}
+
+function mp_dd_get_object_content(int $objectID, $top = null): string
+{
+    $object = get_post($objectID);
+    switch ($object->post_type) {
+        case 'area':
+            return mp_dd_get_area_content($objectID, $top);
+        case 'npc':
+            return mp_dd_get_npc_content($objectID, $top);
+        default:
+            return '';
+    }
+}
+
+function mp_dd_get_area_content(int $areaID, $top = null): string
+{
+    $area = get_post($areaID);
+    ob_start();
+    $postLink = get_permalink($areaID);
+    $topLink = $top !== null ? "<a href='#$top'>&uarr;</a>" : '';
+    ?>
+    <div><a href="<?= $postLink ?>" style="color:inherit;"><h1 id="<?= $areaID ?>" style="display: inline-block;"><?= $area->post_title ?></h1></a> <?= $topLink ?></div>
+    <?= mp_dd_filter_modal_tags(mp_dd_filter_area_content($area->post_content, $area), false, $areaID); ?>
+    <?php
+    return ob_get_clean();
+}
+
+function mp_dd_get_npc_content(int $npcID, $top = null)
+{
+    $npc        = get_post($npcID);
+    $profession = get_post_meta($npcID, 'profession', true);
+    $profession = $profession ? '<span style="margin-left: 5px;"> (' . $profession . ')</span>' : '';
+    $postLink = get_permalink($npcID);
+    $topLink = $top !== null ? "<a href='#$top'>&uarr;</a>" : '';
+    $npcHTML    = "<div><a href='$postLink' style='color:inherit;'><h2 id='$npcID' style=\"display: inline-block;\">$npc->post_title</h2></a> $profession $topLink</div>";
+    if (has_post_thumbnail($npcID)) {
+        $npcHTML .= '<div class="row">';
+        $npcHTML .= '<div class="col s10" style="padding-left: 0;">';
+    }
+    $npcHTML .= '<p>';
+    $class   = get_post_meta($npcID, 'class', true);
+    $level   = get_post_meta($npcID, 'level', true);
+    if ($class && $level) {
+        $npcHTML .= '<b>Class:</b> ' . $class . ' <b>Level:</b> ' . $level . '<br/>';
+    }
+    $npcHTML .= '<b>Height:</b> ' . get_post_meta($npcID, 'height', true) . ' <b>Weight:</b> ' . get_post_meta($npcID, 'weight', true) . '<br/>';
+    $npcHTML .= $npc->post_content . '<br/>';
+    $npcHTML .= '<b>Wearing:</b> ' . get_post_meta($npcID, 'clothing', true) . '<br/>';
+    $npcHTML .= '<b>Possessions:</b> ' . get_post_meta($npcID, 'possessions', true) . '<br/>';
+    $npcHTML .= '</p>';
+    if (has_post_thumbnail($npcID)) {
+        $npcHTML .= '</div>';
+        $npcHTML .= '<div class="col s2 valign-wrapper center-align">';
+        $npcHTML .= get_the_post_thumbnail($npcID, 'thumbnail');
+        $npcHTML .= '</div></div>';
+    }
+
+    return $npcHTML;
+}
+
+
+#endregion
